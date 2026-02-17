56d4fca5f44671bd0255919de916c1874fb8fdfd
Bao Hoang <bao.hoang.22@cnu.edu>
2026-02-17 18:24:49 -0500

    test 2

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  basePath: "/Capstone",
+  assetPrefix: "/Capstone/",
+  trailingSlash: true,
 };
 
 export default nextConfig;
