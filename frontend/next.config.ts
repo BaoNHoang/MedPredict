bfe4cf7e3c44963254dcdd7c28575007dee0f21a
Bao Hoang <bao.hoang.22@cnu.edu>
2026-02-17 18:22:27 -0500

    test

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,17 +1,8 @@
 import type { NextConfig } from "next";
 
-const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] || "";
-const isProd = process.env.NODE_ENV === "production";
-
-const isUserPagesRepo = repo.endsWith(".github.io");
-const basePath = isProd && !isUserPagesRepo ? `/${repo}` : "";
-
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-  basePath,
-  assetPrefix: basePath ? `${basePath}/` : undefined,
-  trailingSlash: true,
 };
 
 export default nextConfig;
